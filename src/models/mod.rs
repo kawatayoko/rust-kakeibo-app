6ccf898264462f33ed64f0d7ab99ec11b352585e
kawata.yoko <kawata.yoko@gmail.com>
2025-12-08 20:46:44 +0900

    feat: 収支集計用のItemヘルパーとsummarizeサービスを追加

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use chrono::NaiveDate;
+use chrono::{NaiveDate, Datelike};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum IncomeCategory {
@@ -49,6 +49,24 @@ impl Item {
                2 => Category::Expense(ExpenseCategory::Other),
                _ => panic!("不正なカテゴリ種別です")
             }
+        }
+    }
+
+    pub fn get_year(&self) -> i32 {
+        self.date.year()
+    }
+
+    pub fn get_month(&self) -> u32 {
+        self.date.month()
+    }
+
+    pub fn get_first_day(&self) -> NaiveDate {
+        NaiveDate::from_ymd(self.get_year(), self.get_month(), 1)
+    }
+    pub fn get_price_for_summary(&self) -> i32 {
+        match self.category {
+            Category::Income(_) => self.price as i32,
+            Category::Expense(_) => -1 * self.price as i32,
         }
     }
 }
